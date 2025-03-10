23692fdb2c1be2f85e42e8fec66e3a060a8ed82e
varun-2024 <v.talwar2024@gmail.com>
2025-03-10 18:10:15 +0000

    Data with EJS

--- backend.js
+++ backend.js
@@ -4,7 +4,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-port = 8080;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
@@ -14,8 +14,9 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 app.get("/ig/:username", (req, res) => {
+  const followers = ["John", "Steve", "Mike", "Doe"];
   let { username } = req.params;
-  res.render("home.ejs", { username });
+  res.render("home.ejs", { username, followers });
 });
 app.get("/rolldice", (req, res) => {
   let diceValue = Math.floor(Math.random() * 6) + 1;
---
+++ instabackend.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on ${port}`);
+});
+app.get("/", (req, res) => {
+  const instaData = require("./data.json");
+  console.log(instaData);
+  res.render("instagram.ejs");
+});
